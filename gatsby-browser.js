fd28bcd1dff3e1e821027e65e70254f05dc2c1d7
Hung Tran <hungtran0203@gmail.com>
2021-06-22 23:21:13 +0700

    + routeStore

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,3 +7,6 @@ import 'firebase/database';
 import 'firebase/firestore';
 import 'firebase/functions';
 import 'firebase/messaging';
+
+import importAll from 'import-all.macro';
+importAll.sync('./**/$route.js');
