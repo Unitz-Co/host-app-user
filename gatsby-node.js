c6424ff6f47f7c291ccb22a477e35515e888da93
Hung Tran <hungtran0203@gmail.com>
2021-06-23 16:14:15 +0700

    + webpack config

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,23 @@ const { gatsbyLoadTemplate } = require('@vl/mod-utils/gatsbyLoadTemplate');
 exports.createPages = (gatsby) => {
   return Promise.all([gatsbyLoadTemplate({ gatsby })]);
 };
+
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: /\.svgx$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 10000,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  });
+};
