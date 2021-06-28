89db35d3e1865dff0985fc189ba99046b6a6c17e
Hung Tran <hungtran0203@gmail.com>
2021-06-28 08:52:24 +0700

    ^ slug route

--- src/templates/advisor/.meta.js
+++ src/templates/advisor/.meta.js
@@ -2,6 +2,34 @@ const path = require('path');
 const _ = require('lodash');
 const { routeStore } = require('@vl/mod-utils/gatsbyRouteStore');
 
+require('@vl/mod-config/web');
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
+const hasuraClient = require('@vl/mod-clients/hasuraCtf');
+
+const getAllAdvisors = async () => {
+  const query = hasuraClient.gql`
+    query advisor {
+      advisor {
+        id
+        profile {
+          id
+          ref_ctf_eid
+        }
+      }
+    }
+  `;
+  try {
+    const rtn = await hasuraClient.getClient().request(query);
+
+    const data = _.get(rtn, 'advisor', []);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+  return [];
+};
+
 exports.createPages = async (item, gatsby) => {
   // @update query
   const allNodes = await gatsby.graphql(`
@@ -15,24 +43,35 @@ exports.createPages = async (item, gatsby) => {
       }
     }
   }`);
-
-  const advisors = _.get(allNodes, 'data.allContentfulAdvisorProfile.nodes', []);
+  const advisors = await getAllAdvisors();
+  const advisorProfiles = _.get(allNodes, 'data.allContentfulAdvisorProfile.nodes', []);
+  const advisorProfilesMapByProfileId = _.keyBy(advisorProfiles, 'id');
 
   return Promise.all(
-    advisors.map((advisor) => {
+    advisors.map((advisorData) => {
+      const profileId = _.get(advisorData, 'profile.ref_ctf_eid');
+      const advisor = {
+        ...advisorData,
+        profile: {
+          ...advisorData.profile,
+          ..._.get(advisorProfilesMapByProfileId, profileId),
+        },
+      };
+
       const advisorSlug = routeStore.toUrl('advisor', advisor);
       const advisorPath = path.join('/', advisorSlug);
       console.log('creating page', advisorPath);
+      const pageContext = _.cloneDeep({
+        id: _.get(advisor, 'id', 'id'),
+        slug: advisorSlug,
+        params: {
+          ...advisor,
+        },
+      });
       return gatsby.actions.createPage({
         path: advisorPath,
         component: item.resolvers.component(gatsby),
-        context: {
-          id: _.get(advisor, 'id', 'id'),
-          slug: advisorSlug,
-          params: {
-            ...advisor,
-          },
-        },
+        context: pageContext,
       });
     })
   );
