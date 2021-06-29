afe74b8f349a4759dad11f18b2386d7c20125623
anhphong-2010 <anhphongnw.2010@gmail.com>
2021-06-29 18:01:33 +0700

    ^search page

--- src/pages/search.js
+++ src/pages/search.js
@@ -10,8 +10,9 @@ import App from '@uz/unitz-app-web/UserApp';
 import Layout from '@uz/unitz-layout-web/LayoutMain';
 
 import PageData from '../data/PageDataQuery';
+import SearchPage from '@uz/unitz-pages/SearchPage';
 
-const SearchIndex = props => {
+const SearchIndex = (props) => {
   return (
     <App>
       <Layout location={props.location} PageData={PageData}>
@@ -19,9 +20,7 @@ const SearchIndex = props => {
           <Helmet title={'siteTitle'} />
           <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'articleNavbarSection' })}</Layout.POS>
           <Layout.POS name="app-body">
-            <div className="wrapper app-row">
-              <h2 className="section-headline">Search</h2>
-            </div>
+            <SearchPage />
           </Layout.POS>
           <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'articleFooterSection' })}</Layout.POS>
         </DIV>
