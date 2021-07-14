efc56e4b4e25a1c48b93b1dc12bf7739f8e1e7b9
Phuong Vo <vlaphuong1991@gmail.com>
2021-07-14 21:50:24 +0700

    video page

--- src/pages/videocall.js
+++ src/pages/videocall.js
@@ -5,25 +5,29 @@ import DIV from '@vl/redata/DIV.macro';
 
 import { Helmet } from 'react-helmet';
 
+import VideoPage from '@uz/unitz-pages/VideoCall';
+
 import App from '@uz/unitz-app-web/UserApp';
 
 import Layout from '@uz/unitz-layout-web/LayoutMain';
 
 import PageData from '../data/PageDataQuery';
 
-const VideoCall = props => {
+const VideoCall = (props) => {
   return (
     <App>
       <Layout location={props.location} PageData={PageData}>
         <DIV>
-          <Helmet title={'siteTitle'} />
-          <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'articleNavbarSection' })}</Layout.POS>
+          <Helmet title={'Video Call'}>
+            <script src="https://static.opentok.com/v2/js/opentok.min.js"></script>
+          </Helmet>
+          {/* <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'articleNavbarSection' })}</Layout.POS> */}
           <Layout.POS name="app-body">
-            <div className="wrapper app-row">
-              <h2 className="section-headline">Search</h2>
+            <div className="relative h-screen overflow-hidden bg-main">
+              <VideoPage />
             </div>
           </Layout.POS>
-          <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'articleFooterSection' })}</Layout.POS>
+          {/* <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'articleFooterSection' })}</Layout.POS> */}
         </DIV>
       </Layout>
     </App>
