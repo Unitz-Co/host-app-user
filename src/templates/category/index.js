779ae2d72e4a910267a5bb7e9c1c9731e990dd99
anhphong-2010 <anhphongnw.2010@gmail.com>
2021-07-15 18:58:09 +0700

    ^detail category page

--- src/templates/category/index.js
+++ src/templates/category/index.js
@@ -8,10 +8,8 @@ import App from '@uz/unitz-app-web/UserApp';
 
 import _ from 'lodash';
 import Layout from '@uz/unitz-layout-web/LayoutMain';
-import { Button } from 'antd';
 import useRoute from '@vl/hooks/useGbRoute';
-
-import gstyles from '@vl/gstyles';
+import DetailCategory from '@uz/unitz-pages/DetailCategory';
 import PageData from '../../data/PageDataQuery';
 
 export const component = (props) => {
@@ -19,7 +17,7 @@ export const component = (props) => {
   // eslint-disable-next-line
   const route = useRoute();
   route.setPageContext(pageContext);
-
+  console.log(pageContext);
   return (
     <App>
       <Layout location={props.location} PageData={PageData}>
@@ -27,44 +25,7 @@ export const component = (props) => {
           <Helmet title={'siteTitle'} />
           <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'articleNavbarSection' })}</Layout.POS>
           <Layout.POS name="app-body">
-            <div className="wrapper app-row">
-              <h2 className="section-headline">Home</h2>
-              <div>
-                <div>
-                  {gstyles.icons({
-                    name: 'apple',
-                    size: 40,
-                  })}
-                </div>
-                <div>Category</div>
-                <div>
-                  <>
-                    <Button type="primary">Primary Button</Button>
-                    <Button>Default Button</Button>
-                    <Button type="dashed">Dashed Button</Button>
-                    <br />
-                    <Button type="text">Text Button</Button>
-                    <Button type="link">Link Button</Button>
-                  </>
-                </div>
-                <div className="text-ellipsis-5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                  officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                  nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
-                  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                  magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                  fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                  deserunt mollit anim id est laborum.
-                </div>
-              </div>
-            </div>
+            <DetailCategory />
           </Layout.POS>
           <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'articleFooterSection' })}</Layout.POS>
         </DIV>
