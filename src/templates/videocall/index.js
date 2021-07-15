802cef18d2740695e8be62cb6b38116b278b5356
Hung Tran <hungtran0203@gmail.com>
2021-07-15 16:30:02 +0700

    ^ binding data for videocall

--- src/templates/videocall/index.js
+++ src/templates/videocall/index.js
@@ -5,13 +5,20 @@ import DIV from '@vl/redata/DIV.macro';
 
 import { Helmet } from 'react-helmet';
 import App from '@uz/unitz-app-web/UserApp';
-// import VideoPage from '@uz/unitz-pages/VideoCall';
+import ServiceSession from '@uz/unitz-pages/ServiceSession';
 
+import _ from 'lodash';
 import Layout from '@uz/unitz-layout-web/LayoutMain';
+import useRoute from '@vl/hooks/useGbRoute';
 
 import PageData from '../../data/PageDataQuery';
 
 export const component = (props) => {
+  const pageContext = _.get(props, 'pageContext');
+  // eslint-disable-next-line
+  const route = useRoute();
+  route.setPageContext(pageContext);
+
   return (
     <App>
       <Layout location={props.location} PageData={PageData}>
@@ -20,8 +27,9 @@ export const component = (props) => {
           <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'articleNavbarSection' })}</Layout.POS>
           <Layout.POS name="app-body">
             <div className="wrapper app-row">
-              <h2 className="section-headline">Welcome To Video Page</h2>
-              <div className="relative h-screen overflow-hidden bg-main">{/* <VideoPage /> */}</div>
+              <div className="relative h-screen overflow-hidden">
+                <ServiceSession />
+              </div>
             </div>
           </Layout.POS>
           <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'articleFooterSection' })}</Layout.POS>
