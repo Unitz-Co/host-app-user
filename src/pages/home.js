73c096e55a58ba3ecf37379944e638ef151c7aeb
Hung Tran <hungtran0203@gmail.com>
2021-07-17 18:06:51 +0700

    #eslint

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,6 +12,9 @@ import Layout from '@uz/unitz-layout-web/LayoutMain';
 import PageData from '../data/PageDataQuery';
 
 const HomeIndex = (props) => {
+  if (props == false) {
+    console.log('testa');
+  }
   return (
     <App>
       <Layout location={props.location} PageData={PageData}>
