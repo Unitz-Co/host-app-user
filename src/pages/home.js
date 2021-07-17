da6674c5190f4fe491b9309a60fd95ed553bab4c
Hung Tran <hungtran0203@gmail.com>
2021-07-17 17:46:44 +0700

    #eslint

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,7 +12,7 @@ import Layout from '@uz/unitz-layout-web/LayoutMain';
 import PageData from '../data/PageDataQuery';
 
 const HomeIndex = (props) => {
-  if (props == true) {
+  if (props == false) {
     console.log('testa');
   }
   return (
