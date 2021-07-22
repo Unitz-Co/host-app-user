4d4fe314a9e2c18a69806df7f5c370ba9ea62d9e
Phuong Vo <vlaphuong1991@gmail.com>
2021-07-22 20:13:12 +0700

    voice page

--- src/templates/voicecall/index.js
+++ src/templates/voicecall/index.js
@@ -5,12 +5,20 @@ import DIV from '@vl/redata/DIV.macro';
 
 import { Helmet } from 'react-helmet';
 import App from '@uz/unitz-app-web/UserApp';
+import VoiceServiceSession from '@uz/unitz-pages/VoiceServiceSession';
 
+import _ from 'lodash';
 import Layout from '@uz/unitz-layout-web/LayoutMain';
+import useRoute from '@vl/hooks/useGbRoute';
 
 import PageData from '../../data/PageDataQuery';
 
 export const component = (props) => {
+  const pageContext = _.get(props, 'pageContext');
+  // eslint-disable-next-line
+  const route = useRoute();
+  route.setPageContext(pageContext);
+
   return (
     <App>
       <Layout location={props.location} PageData={PageData}>
@@ -19,7 +27,9 @@ export const component = (props) => {
           <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'articleNavbarSection' })}</Layout.POS>
           <Layout.POS name="app-body">
             <div className="wrapper app-row">
-              <h2 className="section-headline">Welcome To VoiceCall Page</h2>
+              <div className="relative overflow-hidden">
+                <VoiceServiceSession />
+              </div>
             </div>
           </Layout.POS>
           <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'articleFooterSection' })}</Layout.POS>
