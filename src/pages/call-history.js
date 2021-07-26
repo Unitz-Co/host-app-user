27984bea66ce24ce4cf990189a0c426378b7ed0c
Phuong Vo <vlaphuong1991@gmail.com>
2021-07-26 09:28:55 +0700

    wip

---
+++ src/pages/call-history.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { ctx } from '@vl/redata';
+import DIV from '@vl/redata/DIV.macro';
+
+import { Helmet } from 'react-helmet';
+import App from '@uz/unitz-app-web/UserApp';
+
+import Layout from '@uz/unitz-layout-web/LayoutMain';
+
+import CallHistory from '@uz/unitz-pages/CallHistory';
+import PageData from '../data/PageDataQuery';
+
+const AdvisorIndex = (props) => {
+  return (
+    <App>
+      <Layout location={props.location} PageData={PageData}>
+        <DIV>
+          <Helmet title={'siteTitle'} />
+          <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'articleNavbarSection' })}</Layout.POS>
+          <Layout.POS name="app-body">
+            <div className="wrapper app-row">
+              <CallHistory />
+            </div>
+          </Layout.POS>
+          <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'articleFooterSection' })}</Layout.POS>
+        </DIV>
+      </Layout>
+    </App>
+  );
+};
+
+export default AdvisorIndex;
