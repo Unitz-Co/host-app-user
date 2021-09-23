ede32d4f8198b3c4560390f91620b0291bafbafb
anhphong-2010 <anhphongnw.2010@gmail.com>
2021-09-23 18:00:08 +0700

    ^ SEO title advisor

--- src/templates/advisor/index.js
+++ src/templates/advisor/index.js
@@ -5,19 +5,28 @@ import DIV from '@vl/redata/DIV.macro';
 
 import App from '@uz/unitz-app-web/UserApp';
 import SEO from '@uz/unitz-layout-web/SEO';
-
+import _ from 'lodash';
 import Layout from '@uz/unitz-layout-web/LayoutMain';
 import AdvisorProfile from '@uz/unitz-pages/AdvisorProfile';
 import withPageContext from '@uz/unitz-pages/withPageContext';
-
+import useRoute from '@vl/hooks/useGbRoute';
 import PageData from '../../data/PageDataQuery';
 
 export const component = withPageContext((props) => {
+  const pageContext = useRoute().getPageContext();
   return (
     <App>
       <Layout location={props.location} PageData={PageData}>
         <DIV>
-          <SEO pageData={ctx.apply('ctf.findPage', { name: 'Homepage' })} />
+          <SEO
+            pageData={{
+              ...ctx.apply('ctf.findPage', { name: 'Homepage' }),
+              seoTitle: `${_.get(pageContext, 'params.profile.displayName')} | Unitz`,
+              seoMetaDescription: {
+                seoMetaDescription: `${_.get(pageContext, 'params.profile.displayName')}`,
+              },
+            }}
+          />
           <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'articleNavbarSection' })}</Layout.POS>
           <Layout.POS name="app-body">
             <div className="wrapper app-row">
