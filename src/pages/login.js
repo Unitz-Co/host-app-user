bb0c5f18a46a7aee7dd165773a46cea5746b3e33
Hung Tran <hungtran0203@gmail.com>
2021-10-16 13:35:23 +0700

    local origin auth

---
+++ src/pages/login.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { ctx } from '@vl/redata';
+import DIV from '@vl/redata/DIV.macro';
+
+import LoginPage from '@uz/unitz-pages/LoginPage';
+
+import App from '@uz/unitz-app-web/UserApp';
+import SEO from '@uz/unitz-layout-web/SEO';
+
+import Layout from '@uz/unitz-layout-web/LayoutMain';
+import withPageContext from '@uz/unitz-pages/withPageContext';
+import PageData from '../data/PageDataQuery';
+
+const LoginIndex = withPageContext((props) => {
+  return (
+    <App>
+      <Layout location={props.location} PageData={PageData}>
+        <DIV>
+          <SEO pageData={ctx.apply('ctf.findPage', { name: 'AuthPage' })} />
+          <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'authNavbarSection' })}</Layout.POS>
+          <Layout.POS name="app-body">
+            <LoginPage />
+          </Layout.POS>
+          <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'authFooterSection' })}</Layout.POS>
+        </DIV>
+      </Layout>
+    </App>
+  );
+});
+
+export default LoginIndex;
