79768e46f4432b5ca7703ecad9d845a852f1a803
anhphong-2010 <anhphongnw.2010@gmail.com>
2021-11-05 18:04:21 +0700

    ^ instructor filtering page

---
+++ src/pages/education/advisor-filter.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { ctx } from '@vl/redata';
+import DIV from '@vl/redata/DIV.macro';
+
+import EducationAdvisorFilter from '@uz/unitz-pages/EducationAdvisorFilter';
+import withPageContext from '@uz/unitz-pages/withPageContext';
+import App from '@uz/unitz-app-web/UserApp';
+import SEO from '@uz/unitz-layout-web/SEO';
+
+import Layout from '@uz/unitz-layout-web/LayoutMain';
+
+import PageData from '../../data/PageDataQuery';
+
+const AdvisorFilterIndex = withPageContext((props) => {
+  return (
+    <App>
+      <Layout location={props.location} PageData={PageData}>
+        <DIV>
+          <SEO pageData={ctx.apply('ctf.findPage', { name: 'Homepage' })} />
+          <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'articleNavbarSection' })}</Layout.POS>
+          <Layout.POS name="app-body">
+            <EducationAdvisorFilter />
+          </Layout.POS>
+          <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'articleFooterSection' })}</Layout.POS>
+        </DIV>
+      </Layout>
+    </App>
+  );
+});
+
+export default AdvisorFilterIndex;
