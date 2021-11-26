2696add825bfd454ee2b3ae9c4d62762f32e4d6f
anhphong-2010 <anhphongnw.2010@gmail.com>
2021-11-26 16:10:31 +0700

    naming

---
+++ src/pages/life-style.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { ctx } from '@vl/redata';
+import DIV from '@vl/redata/DIV.macro';
+
+import HomePage from '@uz/unitz-pages/HomeLifeStyle';
+import withPageContext from '@uz/unitz-pages/withPageContext';
+import App from '@uz/unitz-app-web/UserApp';
+
+import Layout from '@uz/unitz-layout-web/LayoutMain';
+import SEO from '@uz/unitz-layout-web/SEO';
+
+import PageData from '../data/PageDataQuery';
+
+const HomeIndex = withPageContext((props) => {
+  return (
+    <App>
+      <Layout location={props.location} PageData={PageData}>
+        <DIV>
+          <SEO pageData={ctx.apply('ctf.findPage', { name: 'HomePageLifeStyle' })} />
+          <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'articleNavbarSection' })}</Layout.POS>
+          <Layout.POS name="app-body">
+            <HomePage />
+          </Layout.POS>
+          <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'FooterSection' })}</Layout.POS>
+        </DIV>
+      </Layout>
+    </App>
+  );
+});
+
+export default HomeIndex;
