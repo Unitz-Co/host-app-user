905d54a5011bcbb73f83d0fdfeec04cb83e15751
Phuong Vo <vlaphuong1991@gmail.com>
2022-01-07 18:54:00 +0700

    update

--- src/templates/me/.route.js
+++ src/templates/me/.route.js
@@ -23,3 +23,25 @@ routeStore.addRule('activities-history-detail', {
     return `/me/activities-history-detail?id=${_.get(params, 'id')}`;
   },
 });
+
+routeStore.addRule('meCourseDetail', {
+  url: (params) => {
+    if (process.env.GATSBY_APP_ENV && _.get(params, 'slug')) {
+      return `/me/courses/${_.get(params, 'slug')}`;
+    }
+
+    let search = `${querystring.stringify(_.pick(params, ['id']))}`;
+    search = search ? `?${search}` : '';
+    return `/me/course${search}`;
+  },
+  parse: (urlObject) => {
+    const params = {};
+    for (let param in urlObject.searchParams) {
+      params[param] = urlObject.searchParams.get(param);
+    }
+    return params;
+  },
+  match: (urlObject) => {
+    return urlObject.pathname === 'me/course';
+  },
+});
