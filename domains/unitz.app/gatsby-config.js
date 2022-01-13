e5e0e193792a546620ee630466bdc59f05083524
Hung Tran <hungtran0203@gmail.com>
2022-01-13 09:49:53 +0700

    disable option for category

--- domains/unitz.app/gatsby-config.js
+++ domains/unitz.app/gatsby-config.js
@@ -1,5 +1,61 @@
+const _ = require('lodash');
+
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || process.env.CONTENTFUL_DELIVERY_TOKEN,
+};
+
+if (process.env.CONTENTFUL_HOST) {
+  contentfulConfig.host = process.env.CONTENTFUL_HOST;
+  contentfulConfig.accessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
+}
+
+const { spaceId, accessToken } = contentfulConfig;
+
+if (!spaceId || !accessToken) {
+  throw new Error('Contentful spaceId and the access token need to be provided.');
+}
+
 module.exports = {
   siteMetadata: {
     title: 'Unitz App',
   },
+  pathPrefix: '/gatsby-contentful-starter',
+  plugins: [
+    'gatsby-transformer-remark',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: contentfulConfig,
+    },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID_WEBSITE,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN_WEBSITE,
+      },
+    },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID_APP,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN_APP,
+        contentItemFilter: (item) => {
+          const contentType = _.get(item, 'sys.contentType.sys.id');
+          const disabled = _.get(item, 'fields.disabled.en-US');
+          if (contentType === 'category') {
+            if (disabled) {
+              return false;
+            }
+          }
+          return true;
+        },
+      },
+    },
+  ],
 };
--- domains/unitz.vn/gatsby-config.js
+++ domains/unitz.vn/gatsby-config.js
@@ -5,18 +5,6 @@ const contentfulConfig = {
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || process.env.CONTENTFUL_DELIVERY_TOKEN,
 };
 
-// If you want to use the preview API please define
-// CONTENTFUL_HOST and CONTENTFUL_PREVIEW_ACCESS_TOKEN in your
-// environment config.
-//
-// CONTENTFUL_HOST should map to `preview.contentful.com`
-// CONTENTFUL_PREVIEW_ACCESS_TOKEN should map to your
-// Content Preview API token
-//
-// For more information around the Preview API check out the documentation at
-// https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
-//
-// To change back to the normal CDA, remove the CONTENTFUL_HOST variable from your environment.
 if (process.env.CONTENTFUL_HOST) {
   contentfulConfig.host = process.env.CONTENTFUL_HOST;
   contentfulConfig.accessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
