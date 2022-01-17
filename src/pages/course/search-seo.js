88ce4b85a440657e1701811b52450f7a96bac7f6
Hung Tran <hungtran0203@gmail.com>
2022-01-17 14:52:57 +0700

    search-seo

---
+++ src/pages/course/search-seo.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { ctx } from '@vl/redata';
+import DIV from '@vl/redata/DIV.macro';
+
+import EducationCourseFilter from '@uz/unitz-pages/EducationCourseFilter';
+import withPageContext from '@uz/unitz-pages/withPageContext';
+import App from '@uz/unitz-app-web/UserApp';
+import SEO from '@uz/unitz-layout-web/SEO';
+
+import Layout from '@uz/unitz-layout-web/LayoutMain';
+import { Helmet } from 'react-helmet';
+
+import PageData from '../../data/PageDataQuery';
+
+const FilterIndex = withPageContext((props) => {
+  return (
+    <>
+      <Helmet>
+        <link rel="stylesheet" type="text/css" href="/seo.css" />
+      </Helmet>
+      <App>
+        <Layout location={props.location} PageData={PageData}>
+          <DIV>
+            <SEO pageData={ctx.apply('ctf.findPage', { name: 'Homepage' })} />
+            <Layout.POS name="app-header">
+              {ctx.apply('ctf.renderSection', { name: 'articleNavbarSection' })}
+            </Layout.POS>
+            <Layout.POS name="app-body">
+              <EducationCourseFilter />
+            </Layout.POS>
+            <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'FooterSection' })}</Layout.POS>
+          </DIV>
+        </Layout>
+      </App>
+    </>
+  );
+});
+
+export default FilterIndex;
