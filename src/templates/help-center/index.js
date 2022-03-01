3b03eed9d30a964d21854b4debe8671ec0a948ae
anhphong-2010 <anhphongnw.2010@gmail.com>
2022-03-01 16:41:41 +0700

    ^ user help center page

--- src/templates/help-center/index.js
+++ src/templates/help-center/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
-import withPageContext from '@uz/unitz-pages/withPageContext';
-import App from '@uz/unitz-app-web/UserApp';
-import Layout from '@uz/unitz-layout-web/LayoutUserHelpCenter';
-import PageData from '../../data/PageDataQuery';
-import DIV from '@vl/redata/DIV.macro';
-import SEO from '@uz/unitz-layout-web/SEO';
 import { ctx } from '@vl/redata';
-import HelpCenterPage from '@uz/unitz-pages/HelpCenter';
+import DIV from '@vl/redata/DIV.macro';
 
-const HelpCenterIndex = withPageContext((props) => {
+import App from '@uz/unitz-app-web/UserApp';
+import SEO from '@uz/unitz-layout-web/SEO';
+
+import Layout from '@uz/unitz-layout-web/LayoutUserHelpCenter';
+import HelpCenter from '@uz/unitz-pages/HelpCenter';
+import useRoute from '@vl/hooks/useGbRoute';
+import withPageContext from '@uz/unitz-pages/withPageContext';
+import _ from 'lodash';
+import PageData from '../../data/PageDataQuery';
+
+export const component = withPageContext((props) => {
+  const pageContext = useRoute().getPageContext();
+
   return (
     <App>
       <Layout location={props.location} PageData={PageData}>
@@ -17,7 +23,7 @@ const HelpCenterIndex = withPageContext((props) => {
           <SEO pageData={ctx.apply('ctf.findPage', { name: 'Homepage' })} />
           <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'articleNavbarSection' })}</Layout.POS>
           <Layout.POS name="app-body">
-            <HelpCenterPage />
+            <HelpCenter />
           </Layout.POS>
           <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'FooterSection' })}</Layout.POS>
         </DIV>
@@ -26,4 +32,4 @@ const HelpCenterIndex = withPageContext((props) => {
   );
 });
 
-export default HelpCenterIndex;
+export default component;
