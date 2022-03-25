21a6c6d168e9fd14df64e70004bcc2f1bd4f5978
ehphuong <vlaphuong1991@gmail.com>
2022-03-25 10:53:00 +0700

    hotfix canvas

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,6 +18,10 @@ exports.onCreateWebpackConfig = ({ stage, actions, loaders, getConfig }) => {
             test: /fabric/,
             use: loaders.null(),
           },
+          {
+            test: /canvas/,
+            use: loaders.null(),
+          },
         ],
       },
     });
