4415e230ee2dc77a07cfa01d41435f08e873179b
anhphong-2010 <anhphongnw.2010@gmail.com>
2022-06-17 17:31:35 +0700

    ^ request log page

---
+++ src/pages/me/request-log.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { ctx } from '@vl/redata';
+import DIV from '@vl/redata/DIV.macro';
+
+import App from '@uz/unitz-app-web/UserApp';
+import SEO from '@uz/unitz-layout-web/SEO';
+
+import Layout from '@uz/unitz-layout-web/LayoutUserProfile';
+import withPageContext from '@uz/unitz-pages/withPageContext';
+import UserRequestLog from '@uz/unitz-pages/UserRequestLog';
+
+import PageData from '../../data/PageDataQuery';
+
+const RequestLogIdx = withPageContext((props) => {
+  return (
+    <App>
+      <Layout location={props.location} PageData={PageData}>
+        <DIV>
+          <SEO pageData={ctx.apply('ctf.findPage', { name: 'Homepage' })} />
+          <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'articleNavbarSection' })}</Layout.POS>
+          <Layout.POS name="app-body">
+            <div className="min-h-full bg-background1">
+              <UserRequestLog />
+            </div>
+          </Layout.POS>
+          <Layout.POS name="app-footer">
+            <div className="hidden min-lg:block">{ctx.apply('ctf.renderSection', { name: 'FooterSection' })}</div>
+          </Layout.POS>
+        </DIV>
+      </Layout>
+    </App>
+  );
+});
+
+export default RequestLogIdx;
